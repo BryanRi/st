c3192cf77e3366a4d50a4d621aa25f2a184ed91c
Bryan Rinders <Your-Email>
2022-06-26 13:52:19 +0200

    Add invert patch

--- config.def.h
+++ config.def.h
@@ -205,6 +205,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	// INVERT PATCH
+	{ TERMMOD,              XK_R,           invert,         { }       },
         // SCROLLBACK PATCH
 	{ ControlMask,          XK_e,           kscrollup,      {.i =  1} },
 	{ ControlMask,          XK_n,           kscrolldown,    {.i =  1} },
